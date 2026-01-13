102e8df888a86306e91ae6d46fddb2cdaaed6dd6
Ho Thai Hung <thaihung.ivac@gmail.com>
2026-01-13 09:43:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -17,8 +16,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          // Thay thế __dirname bằng path.resolve('.') để tránh lỗi không tìm thấy __dirname trong ESM
-          '@': path.resolve('.'),
+          '@': path.resolve(__dirname, '.'),
         }
       }
     };
